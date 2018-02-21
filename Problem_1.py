1a84eae6576d2c70eb9ab1ee71f49f26c20ca610
David Erroll <daviderroll@Macintosh.local>
2018-02-21 10:00:41 -0500

    Start

---
+++ Problem_1.py
@@ -0,0 +1,14 @@
+# Problem 1
+
+# If we list all the natural numbers below 10 that are multiples of 3 or 5, we get 3, 5, 6 and 9. The sum of these multiples is 23.
+
+# Find the sum of all the multiples of 3 or 5 below 1000.
+
+
+
+# I'm not sure there is a way to confirm all multiples without checking each digit.
+# This loop checks all digits:
+
+for n in range(1, 1001):
+
+    
