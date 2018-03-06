b06bc90df7caccb829cd514eaa4e629edb8107ba
David Erroll <DavidErroll@users.noreply.github.com>
2018-03-06 11:17:55 -0500

    update

--- Problem_3.py
+++ Problem_3.py
@@ -7,3 +7,28 @@
 # Since prime factorization is the basis of current encryption methods, I am confident that:
 #   1. a brute-force loop of some type is necessary; and
 #   2. people who know how to optimize code for speed will have written superior loops.
+
+from math import sqrt
+
+test_value = 600851475143
+test_check = 0
+
+while not test_check:
+
+  test_divisor = 2
+  test_factor = test_value / test_divisor
+
+  if str(test_factor) in ['2', '3', '5', '7', '11', '13']:
+    test_check = 1
+    return test_factor
+
+  elif str(test_factor)[-1] in ['0', '2', '4', '5', '6', '8'] or sum(int(digit) for digit in str(test_factor)) % 3 == 0 or 7 or 11:
+    test_divisor += 1
+
+  else:
+    brute_divisor = test_factor // 7
+    while test_factor % brute_divisor != 0 or brute_divisor == 1:
+      brute_divisor = brute_divisor - 1
+      test_check = 1
+      return brute_divisor
+
